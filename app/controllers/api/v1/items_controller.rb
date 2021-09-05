ce0043ce84ea8c1f5624883ca90da07ea33e9a95
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-04 21:56:09 -0600

    Add create and destroy methods

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,4 +10,18 @@ class Api::V1::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     render json: ItemSerializer.new(@item)
   end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
