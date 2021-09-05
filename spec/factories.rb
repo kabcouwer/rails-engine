8e716ebdca13ca0ed38bdcb2138c4f0b7498c606
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-04 20:09:40 -0600

    Add item to factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,11 @@ FactoryBot.define do
   factory :merchant do
     name { Faker::DcComics.name }
   end
+
+  factory :item do
+    name { Faker::Fantasy::Tolkien.character }
+    description { Faker::Fantasy::Tolkien.poem }
+    unit_price { Faker::Number.binary(digits: 5) }
+    merchant
+  end
 end
