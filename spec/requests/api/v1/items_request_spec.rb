41c88ee35aacf854abc8c27090491f4b508824c2
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-04 21:13:06 -0600

    Add tests for items index and show page

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -3,20 +3,40 @@ require 'rails_helper'
 describe 'Items API' do
   describe 'happy paths' do
     before :each do
-      merchant1 = create(:merchant)
-      merchant2 = create(:merchant)
+      @merchant1 = create(:merchant)
+      @merchant2 = create(:merchant)
+      @merchant3 = create(:merchant)
+      @merchant4 = create(:merchant)
 
-      item1 = create(:item, merchant: merchant1)
-      item2 = create(:item, merchant: merchant1)
-      item3 = create(:item, merchant: merchant1)
-      item4 = create(:item, merchant: merchant1)
-      item5 = create(:item, merchant: merchant1)
-      item6 = create(:item, merchant: merchant2)
-      item7 = create(:item, merchant: merchant2)
-      item8 = create(:item, merchant: merchant2)
+
+      @item1 = create(:item, merchant: @merchant1)
+      @item2 = create(:item, merchant: @merchant1)
+      @item3 = create(:item, merchant: @merchant1)
+      @item4 = create(:item, merchant: @merchant1)
+      @item5 = create(:item, merchant: @merchant1)
+      @item6 = create(:item, merchant: @merchant1)
+      @item7 = create(:item, merchant: @merchant1)
+      @item8 = create(:item, merchant: @merchant2)
+      @item9 = create(:item, merchant: @merchant2)
+      @item10 = create(:item, merchant: @merchant2)
+      @item11 = create(:item, merchant: @merchant2)
+      @item12 = create(:item, merchant: @merchant2)
+      @item13 = create(:item, merchant: @merchant2)
+      @item14 = create(:item, merchant: @merchant2)
+      @item15 = create(:item, merchant: @merchant3)
+      @item16 = create(:item, merchant: @merchant3)
+      @item17 = create(:item, merchant: @merchant3)
+      @item18 = create(:item, merchant: @merchant3)
+      @item19 = create(:item, merchant: @merchant3)
+      @item20 = create(:item, merchant: @merchant3)
+      @item21 = create(:item, merchant: @merchant4)
+      @item22 = create(:item, merchant: @merchant4)
+      @item23 = create(:item, merchant: @merchant4)
+      @item24 = create(:item, merchant: @merchant4)
+      @item25 = create(:item, merchant: @merchant4)
     end
 
-    it 'gets all items' do
+    it 'gets all items, max of 20 per page' do
       get '/api/v1/items'
 
       expect(response).to be_successful
@@ -24,7 +44,7 @@ describe 'Items API' do
       body = JSON.parse(response.body, symbolize_names: true)
       items = body[:data]
 
-      expect(items.count).to eq(5)
+      expect(items.count).to eq(20)
 
       items.each do |item|
         expect(item).to have_key(:id)
@@ -39,7 +59,79 @@ describe 'Items API' do
 
         expect(item[:attributes]).to have_key(:name)
         expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
       end
+    end
+
+    it 'returns first 20 items and page 1 as default' do
+      get '/api/v1/items'
+
+      expect(response).to be_successful
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      items = body[:data]
+
+      expect(items.count).to eq(20)
+      expect(items.first[:attributes][:name]).to eq(Item.first.name)
+      expect(items.last[:attributes][:name]).to eq(Item.all[19].name)
+    end
+
+    it 'returns correct items with per page and page query params' do
+      get '/api/v1/items?per_page=10&page=2'
+
+      expect(response).to be_successful
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      items = body[:data]
+
+      expect(items.count).to eq(10)
+      expect(items.first[:attributes][:name]).to eq(Item.all[10].name)
+      expect(items.last[:attributes][:name]).to eq(Item.all[19].name)
+    end
+
+    it 'gets one merchant' do
+      get "/api/v1/items/#{@item1.id}"
+
+      expect(response).to be_successful
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      item = body[:data]
+
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item[:id]).to eq(@item1.id.to_s)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to eq('item')
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to eq(@item1.name)
+      expect(item[:attributes][:description]).to eq(@item1.description)
+      expect(item[:attributes][:unit_price]).to eq(@item1.unit_price)
+    end
+  end
+
+  describe 'sad paths' do
+    it 'returns data as an empty array if query results in no data' do
+      get '/api/v1/items?per_page=25&page=2'
+
+      expect(response).to be_successful
+
+      body = JSON.parse(response.body, symbolize_names: true)
+      items = body[:data]
+
+      expect(items).to eq([])
+    end
+
+    xit 'returns 404 with bad merchant id' do
+      get '/api/v1/items/12345'
+
+      expect(response.status).to eq(404)
     end
   end
 end
