87f3f5c068b2361e64df99aa01c1d049227359d3
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-04 20:08:36 -0600

    Add show method

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    @page = params[:page]
-    @per_page = params[:per_page]
-    @merchants = Merchant.paginate(page: @page, per_page: @per_page)
+    page = params[:page]
+    per_page = params[:per_page]
+    @merchants = Merchant.paginate(page: page, per_page: per_page)
     render json: MerchantSerializer.new(@merchants)
+  end
+
+  def show
+    @merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(@merchant)
   end
 end
