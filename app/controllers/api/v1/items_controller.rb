cd8333dff340517a87e94fdffdc39b9f917fa393
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-06 16:02:49 -0600

    Update controller with page 0 and per_page 0 option, and update method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,31 +1,42 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    page = params[:page]
-    per_page = params[:per_page]
-    @items = Item.paginate(page: page, per_page: per_page)
-    render json: ItemSerializer.new(@items)
+    page = params[:page].to_i.zero? ? 1 : params[:page].to_i
+    per_page = params[:per_page].to_i.zero? ? 20 : params[:per_page].to_i
+    items = Item.paginate(page: page, per_page: per_page)
+    render json: ItemSerializer.new(items)
   end
 
   def show
-    @item = Item.find(params[:id])
-    render json: ItemSerializer.new(@item)
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item)
   rescue ActiveRecord::RecordNotFound
     not_found
   end
 
   def create
-    @item = Item.create(item_params)
-    if @item.save
-      render json: ItemSerializer.new(@item), status: :created
+    item = Item.create(item_params)
+    if item.save
+      render json: ItemSerializer.new(item), status: :created
     else
-      render json: { errors: @item.errors.full_messages },
-             status: :unprocessable_entity
+      bad_request(item.errors.full_messages)
     end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: ItemSerializer.new(item)
+    else
+      bad_request(item.errors.full_messages)
+    end
+  rescue ActiveRecord::RecordNotFound
+    not_found
   end
 
   def destroy
     item = Item.find(params[:id])
     item.destroy
+    head :no_content
   rescue ActiveRecord::RecordNotFound
     not_found
   end
