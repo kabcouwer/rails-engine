d817618a06411c49360f4786627b43aedeb4f4f8
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-06 16:03:32 -0600

    Create bad_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::API
   def not_found
-    render json: { error: "not_found" }, status: :not_found
+    render json: { error: 'Not found' }, status: :not_found
+  end
+
+  def bad_request(errors)
+    render json: { errors: errors }, status: :bad_request
   end
 end
