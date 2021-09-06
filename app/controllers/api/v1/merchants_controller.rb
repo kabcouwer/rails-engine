5c5dd3bdb914b7a451ec2b5d12e5c7e00158cbb3
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-06 16:03:09 -0600

    Update controller with page 0 and per_page 0 option

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    page = params[:page]
-    per_page = params[:per_page]
-    @merchants = Merchant.paginate(page: page, per_page: per_page)
-    render json: MerchantSerializer.new(@merchants)
+    page = params[:page].to_i.zero? ? 1 : params[:page].to_i
+    per_page = params[:per_page].to_i.zero? ? 20 : params[:per_page].to_i
+    merchants = Merchant.paginate(page: page, per_page: per_page)
+    render json: MerchantSerializer.new(merchants)
   end
 
   def show
-    @merchant = Merchant.find(params[:id])
-    render json: MerchantSerializer.new(@merchant)
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   rescue ActiveRecord::RecordNotFound
     not_found
   end
