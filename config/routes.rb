a43df3776e6dde340859b2aa37969ea1219bf626
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-06 22:53:02 -0600

    Add items find all path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'merchants/find', to: 'merchants/search#find'
+      get 'items/find_all', to: 'items/search#find_all'
       resources :merchants, only: %i[index show] do
         resources :items, module: 'merchants', only: [:index]
       end
