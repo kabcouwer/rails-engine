b3f0c4cc0481e2fbbb66d327baac6c763a5eb81c
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-06 22:52:51 -0600

    Add search class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,8 @@ class Item < ApplicationRecord
   validates :unit_price, presence: true, numericality: true
   validates :merchant_id, presence: true
   validates :merchant, presence: true
+
+  def self.search(search_params)
+    where('name ILIKE ? or description ILIKE ?', "%#{search_params}%", "%#{search_params}%")
+  end
 end
